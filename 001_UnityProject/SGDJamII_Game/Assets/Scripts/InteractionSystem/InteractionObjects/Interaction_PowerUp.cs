b8ae2ab7ebad26d6200e97e67a49ac0499a9f8ef
sergiosev123 <sergiotraceslava@gmail.com>
2020-09-24 18:53:51 +0200

    ADDED - Power Ups created and implemented. ADDED - Power Up selection scene created.

---
+++ 001_UnityProject/SGDJamII_Game/Assets/Scripts/InteractionSystem/InteractionObjects/Interaction_PowerUp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interaction_PowerUp : InteractableBase
+{
+    [Header("Power Up Settings")]
+    [SerializeField] private PowerUp powerUp;
+
+    private Player m_player;
+
+    public delegate void SelectionAction();
+    public static event SelectionAction OnSelection;
+
+    #region Built-In Methods
+
+    private void Awake()
+    {
+        if (powerUp == null)
+        {
+            Debug.LogError(gameObject.name + " object doesnt have PowerUp");
+            return;
+        }
+
+        ActionName = powerUp.PowerUpName;
+    }
+
+    private void Start()
+    {
+        m_player = GameObject.FindObjectOfType<Player>();
+
+        if (!m_player)
+            Debug.LogError("Player not found.");
+    }
+
+    #endregion
+
+    
+    public override void OnInteract()
+    {
+        base.OnInteract();
+
+        ApplyPowerUp();
+    }
+
+    void ApplyPowerUp()
+    {
+        /*
+        if (powerUp == null)
+            return;
+        */
+
+        m_player.PowerUp = powerUp;
+        powerUp?.OnApply();
+
+        OnSelection();
+
+        DestroyImmediate(transform.gameObject);
+    }
+}
---
+++ 001_UnityProject/SGDJamII_Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Singleton<Player>
+{
+    [Header("Power Up")]
+    [SerializeField] private PowerUp currentPowerUp;
+
+    #region Properties
+
+    public PowerUp PowerUp
+    {
+        get => currentPowerUp;
+        set => currentPowerUp = value;
+    }
+
+    #endregion
+}
