f22eaea876d4805548c448fcadc207b5dfe0c51a
sgavil <ssjsrgx@gmail.com>
2020-09-27 18:05:43 +0200

    Doing scenes things

---
+++ 001_UnityProject/SGDJamII_Game/Assets/Scripts/ScenesManagers/SceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ChangeScene();
+    }
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
